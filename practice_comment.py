5887a2e5d89d096d9b738bec4fd873133d2a369b
benjamintmc <ben3201991@gmail.com>
2018-11-26 01:28:46 +0800

    filter practice

--- practice_comment.py
+++ practice_comment.py
@@ -10,10 +10,20 @@ with open('reviews.txt', 'r') as f:
 			print(len(data))
 print('Loading finished,', len(data), 'results in total')
 
+# calculat the average length of reviews
 total_len = 0
 for review in data:
 	len(review)
 	total_len += len(review)
-
-
 print('Average review length is', total_len/len(data), 'characters')
+
+# filter review with at least 100 characters
+filter1 = []
+filter2 = []
+for review in data:
+	if len(review) > 100:
+		filter1.append(len(review))
+	elif len(review) <= 100:
+		filter2.append(len(review))
+print(len(filter1), 'reviews have more than 100 characters')
+print(len(filter2), 'reviews have up to 100 characters')
